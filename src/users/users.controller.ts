59ff16bba2728586de8149bb7a6b43b22c87bfbb
Ahmed Khaled <a.k.nasr18@gmail.com>
2025-06-10 10:23:49 +0300

    feat: implement user signup functionality with DTO validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { log } from 'console';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    log('User created:', body);
+  }
+}
